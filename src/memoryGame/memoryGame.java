cead5a538f6f689e157bb6bc241cae3258d31ce4
unknown <Evelin.Jogi@R2320.hm.ee>
2015-11-04 23:51:00 +0200

    Lisasin meetodite nimetused.

--- src/memoryGame/memoryGame.java
+++ src/memoryGame/memoryGame.java
@@ -1,7 +1,19 @@
 package memoryGame;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Created by Evelin.Jogi on 4.11.2015.
  */
-public class memoryGame {
+public class memoryGame extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //seadistaLava();
+        //paigutaPildid();
+        //reageeriKlikile();
+        //pilteOnAlles();
+        
+    }
 }
