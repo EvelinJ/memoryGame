4e179becdb2bface2ef4205a30b08bb45a9a5c53
unknown <Evelin.Jogi@R2320.hm.ee>
2015-12-12 02:17:36 +0200

    Lisasin reageeriklikile ja pilteAlles.

--- src/memoryGame/Laud.java
+++ src/memoryGame/Laud.java
@@ -1,6 +1,7 @@
 package memoryGame;
 
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -35,16 +36,53 @@ public class Laud {
         mang.setTitle("Memoriin");
 
         genereeriPildid();
+        reageeriKlikile();
     }
 
-    private void genereeriPildid() {
+    private void reageeriKlikile() {
+        laud.setOnMouseClicked(event -> {//kuidas teha enne kaks klikki, et if lause käivitada?
+            Rectangle pilt = (Rectangle) event.getTarget();//event ütleb, kes on see, kelle peale klikati ja kasutame rectangle meetodeid
+            String pildiId = pilt.getId();//teeme id1 ja id2 ja siis if lauses kontrollime, kas võrduvad omavahel, selle asemel, et kontrollime kas on Pilt1 või Pilt2?
+            if (pildiId.equals("pilt1")) {//peame siia kahe erineva pildi võrdluse tekitama ja kaks klikki tuleb teha enne hinnangut. If kui on paar.
+                pilt.setFill(Color.WHITESMOKE);
+                pilt.setId("paar");
+            } else if (pildiId.equals("pilt2")) {//if kui ei ole paar
+                pilt.setFill((Color.BLUE));//kui ei olnud paar, siis värvib siniseks tagasi
+                pilt.setId(("arvamata"));//panime praegu, et on arvamata, et game overit kontrollida, kui jätame id pilt2, siis ei jõuagi mängu lõppu pilteAlles meetodis
+            } else if (pildiId.equals("paar")) {
+                System.out.println("Arvatud pilt!");
+            }
+            if (!pilteAlles()) {//kui pilte ei ole alles siis prindib mäng läbi
+                System.out.println("Mäng läbi!");
+                //gameOver();
+            }
+        });
+    }
+
+    private boolean pilteAlles() {
+        for (Node pilt : laud.getChildren()) {
+            if (pilt.getId().equals("pilt1")) {//peaksime tegema nii palju if-e, kui meil erinevaid pilte on
+                return true;//tagastab meetodi tulemuse ehk et pilte on veel alles, kui seda käsku näeb, siis enam edasi ei lähe
+            } else if (pilt.getId().equals("pilt2")) {
+                return true;
+            }
+        }
+        return false;//if käib kõik pildid läbi ja kui ei jõudnud tulemuseni, et pilte on alles, siis tuleb siia
+    }
+
+    private void genereeriPildid() {//tsükkel piltide lauale asetamiseks
         for (int i = 0; i < laualRidasid; i++) {
             for (int j = 0; j < laualTulpasid; j++) {
-                Rectangle pilt = new Rectangle(pildiKylg, pildiKylg);
-                pilt.setId("Pilt");
+                Rectangle pilt = new Rectangle(pildiKylg, pildiKylg);//pildi loomine etteantud mõõtmetega
+                int rand = (int) (Math.random() * 2);//meie peame siia tegema rohkem kui kaks valikut, sest paare on üle kahe, võiks korrutada (*2*laualTulpasid), aga kuidas, siis if tsükkel, teha, et ta nii palju erinevaid pilte annaks?
+                if (rand == 1) {
+                    pilt.setId("pilt1");//pilt1 teab, et on pilt1, aga siia võib igale pildile mingi oma nime panna, et neid erinevalt midagi tegema panna
+                } else {
+                    pilt.setId("pilt2");
+                }
                 pilt.setFill(Color.BLUE);
                 pilt.setStroke(Color.BLACK);//seda võib ka mitte teha
-                laud.setHgap(piltideVahe);
+                laud.setHgap(piltideVahe);//tekitab piltide asetuses vahed, et pildid ei oleks üksteise küljes
                 laud.setVgap(piltideVahe);
                 laud.add(pilt, i, j);
             }
